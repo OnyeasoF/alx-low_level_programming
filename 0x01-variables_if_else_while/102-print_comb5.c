93b347f03c2089a0ec25fd34efcdeb84992ed1bc
OnyeasoF <favouronyeaso@gmail.com>
2022-09-09 21:10:18 -0700

    ‘omo’

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: prints all possible combination of 2-digit numbers
+ * Return: Always 0 (success)
+ */
+int main(void)
+{int c, i, k, j;;
