4290c61db9337b4590ac5b5c757485ed997cd2bf
OnyeasoF <favouronyeaso@gmail.com>
2022-10-20 15:31:51 -0700

    omo

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,40 +1,48 @@
 #include "lists.h"
-#include <strings.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
+
 /**
- *add_node_end - adds a node at the end of a list
- *@head:pointer to head
- *@str:string to duplicate
- *Return:new element
- */
+* add_node_end - Adds a new node at the end of a list_t list.
+* @head: A pointer to the head of the list_t list.
+* @str: The string to be added to the list_t list.
+*
+* Return: If the function fails - NULL.
+*       Otherwise - the address of the new element.
+*/
 list_t *add_node_end(list_t **head, const char *str)
 {
-	int len;
-	list_t *last;
-	list_t *new_node;
+        char *dup;
+        int len;
+        list_t *new, *last;
 
-	last = *head;
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
-		return (NULL);
-	for (len = 0; str[len]; len++)
-		;
-	new_node->str = strdup(str);
-	new_node->len = len;
-	new_node->next = NULL;
-	if ((*head) == NULL)
-	{
-		(*head) = new_node;
-	}
-	else
-	{
-		last = *head;
-		while (last->next != NULL)
-		{
-			last = last->next;
-		}
-		last->next = new_node;
-	}
-	return (new_node);
+        new = malloc(sizeof(list_t));
+        if (new == NULL)
+                return (NULL);
+
+        dup = strdup(str);
+        if (str == NULL)
+        {
+                free(new);
+                return (NULL);
+        }
+
+        for (len = 0; str[len];)
+                len++;
+
+        new->str = dup;
+        new->len = len;
+        new->next = NULL;
+
+        if (*head == NULL)
+                *head = new;
+
+        else
+        {
+                last = *head;
+                while (last->next != NULL)
+                        last = last->next;
+                last->next = new;
+        }
+
+        return (*head);
 }
